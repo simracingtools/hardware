d8d2275a31a30ac5385957eab061b0aea36f2822
rbausdor <robert.bausdorf@directline.de>
2019-04-26 10:28:46 +0200

    Simplified joystick button single press generation

--- starterbox/iracing/config.h
+++ starterbox/iracing/config.h
@@ -33,22 +33,23 @@
 #define JOY_BTN_ENG2_INC    15
 
 // Pin assignment definitions
-#define PIN_SW_IGN     0      // Ignition switch
-#define PIN_BTN_START  1      // Engine starter
-#define PIN_SW_PIT     2      // ACC1 switch
-#define PIN_SW_TCR     3      // ACC2 switch
-#define PIN_BTN_ENG    4      // ENG push
-#define PIN_BTN_ABS    17     // ABS push
-#define PIN_BTN_TCR    18     // TCR push
-#define PIN_LED_RED    10     // LED red, must be PWM capable pin
-#define PIN_LED_BLUE   9      // LED blue, must be PWM capable pin
-#define PINS_ENC_ENG   5,16   // Engine encoder pins
-#define PINS_ENC_ABS   6,15   // ABS encoder pins
-#define PINS_ENC_TCR   7,14   // TCR encoder pins
+#define PIN_SW_IGN           0   // Ignition switch
+#define PIN_BTN_START        1   // Engine starter
+#define PIN_SW_PIT           2   // ACC1 switch
+#define PIN_SW_TCR           3   // ACC2 switch
+#define PIN_BTN_ENG          4   // ENG push
+#define PIN_BTN_ABS         17   // ABS push
+#define PIN_BTN_TCR         18   // TCR push
+#define PIN_LED_RED         10   // LED red, must be PWM capable pin
+#define PIN_LED_BLUE         9   // LED blue, must be PWM capable pin
+#define PINS_ENC_ENG      5,16   // Engine encoder pins
+#define PINS_ENC_ABS      6,15   // ABS encoder pins
+#define PINS_ENC_TCR      7,14   // TCR encoder pins
 
 // Configuration definitions
-#define ENC_STEPS      8      // Default encoder sensitivity
-#define LED_BRIGHTNESS 12     // Default PWM value for LED brightness 
-#define JOYSTICK_MODE  false  // Default operation mode Keyboard
-#define BOUNCE_TIME    10     // Bounce delay for buttons
-#define CONFIG_ADDRESS 0      // EEPROM address for config data
+#define ENC_STEPS            8   // Default encoder sensitivity
+#define LED_BRIGHTNESS      12   // Default PWM value for LED brightness 
+#define JOYSTICK_MODE    false   // Default operation mode Keyboard
+#define BOUNCE_TIME         10   // Bounce delay for buttons
+#define CONFIG_ADDRESS       0   // EEPROM address for config data
+#define JOY_BTN_DURATION   100   // Delay between button press and release
